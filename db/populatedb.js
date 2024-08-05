ce152f8e691784932610742405dbb4a67707f9d7
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-05 17:07:32 +0200

    Refactor populatedb for new sql tables

--- db/populatedb.js
+++ db/populatedb.js
@@ -6,15 +6,17 @@ const mongoDB = userArgs[0];
 const SQL = `
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR ( 255 ),
+  last_name VARCHAR ( 255 ),
+  e_mail VARCHAR ( 255 ),
   username VARCHAR ( 255 ),
-  password VARCHAR ( 255 ),
-  is_admin BOOLEAN
+  password VARCHAR ( 255 )
 );
 
 CREATE TABLE IF NOT EXISTS posts (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   user_id integer,
-  title VARCHAR ( 255 ),
+  likes integer,
   content VARCHAR ( 255 ),
   date TIMESTAMPTZ,
   FOREIGN KEY (user_id) REFERENCES users(id)
@@ -29,6 +31,23 @@ CREATE TABLE IF NOT EXISTS comments (
   date TIMESTAMPTZ,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS followers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id integer,
+  follower integer,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (follower) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS requests (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id integer,
+  request_sender integer,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (request_sender) REFERENCES users(id)
 );
 `;
 
