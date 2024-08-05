abc8d2eec492773ac0a6be88e797fdbd53f632f5
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-05 20:48:35 +0200

    Add comment & post controllers

---
+++ controllers/commentController.js
@@ -0,0 +1,40 @@
+// const Post = require('../models/post')
+const dbComments = require('../db/queries/commentsQueries');
+const { jwtDecode } = require('jwt-decode');
+
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+
+exports.post_comments = asyncHandler(async (req, res) => {
+  const postId = req.params.postid;
+  const postComments = await dbComments.getCommentsByPostId(postId);
+  res.json(postComments);
+});
+
+exports.comment_create_post = [
+  body('content', 'content is required').trim().isLength({ min: 1 }),
+
+  asyncHandler(async (req, res) => {
+    const userId = jwtDecode(req.headers.authorization).sub;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.json(errors.array());
+    } else {
+      const content = req.body.content;
+      const date = new Date();
+      const user = userId;
+      const post = req.params.postid;
+
+      await dbComments.insertComment(content, date, user, post);
+      res.json('Comment saved');
+    }
+  }),
+];
+
+exports.comment_delete = asyncHandler(async (req, res) => {
+  const commentId = req.params.commentid;
+  await dbComments.deleteComment(commentId);
+  res.json('Comment deleted');
+});
---
+++ controllers/postController.js
@@ -0,0 +1,57 @@
+const dbPosts = require('../db/queries/postsQueries');
+const dbComments = require('../db/queries/commentsQueries');
+
+const { jwtDecode } = require('jwt-decode');
+
+const asyncHandler = require('express-async-handler');
+
+const { body, validationResult } = require('express-validator');
+
+exports.all_posts = asyncHandler(async (req, res) => {
+  const allPosts = await dbPosts.getAllPosts();
+  console.log(allPosts);
+  res.json(allPosts);
+});
+
+exports.single_post = asyncHandler(async (req, res) => {
+  const postId = req.params.postid;
+  const post = await dbPosts.getSinglePost(postId);
+  res.json(post);
+});
+
+// create post
+
+exports.post_create_post = [
+  body('title', 'title is required').trim().isLength({ min: 1 }),
+  body('content', 'content is required').trim().isLength({ min: 1 }),
+
+  asyncHandler(async (req, res) => {
+    const userId = jwtDecode(req.headers.authorization).sub;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      return res.json(errors.array());
+    } else {
+      const title = req.body.title;
+      const content = req.body.content;
+      const date = new Date();
+      const user = userId;
+      await dbPosts.insertPost(title, content, date, user);
+
+      res.json('Post saved');
+    }
+  }),
+];
+
+// delete post
+
+exports.post_delete = asyncHandler(async (req, res) => {
+  const postId = req.params.postid;
+  const comment = await dbComments.deleteAllPostsComments(postId);
+  const post = await dbPosts.deletePost(postId);
+
+  console.log('1', post, '2', comment);
+  res.json('Post deleted');
+});
+
